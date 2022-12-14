e445ee2b0742fdbabf2723e7c62ff5116ed5160d
Gustavo <gustavodarocha241@gmail.com>
2022-12-14 16:06:35 -0300

    50% de cobertura em todas as partes

--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -10,10 +10,10 @@ const saleController = require('../../../src/controllers/sales.controller');
 
 const { sales } = require('./mocks/sale.controllers.mock');
 
-describe('testando a camada controller de products', function () {
+describe('testando a camada controller de sales', function () {
   afterEach(sinon.restore);
 
-  it('testando se todos os produtos são retornados', async function () {
+  it('testando se todas as vendas são retornadas', async function () {
     const res = {};
     const req = {};
 
@@ -27,5 +27,23 @@ describe('testando a camada controller de products', function () {
 
     expect(res.status).to.have.been.calledWith(200);
     expect(res.json).to.have.been.calledWith(sales);
+  });
+
+  it('testando se uma venda especifica é retornada', async function () {
+    const res = {};
+    const req = {
+      params: { id: 1 },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(saleService, 'findById')
+      .resolves({ type: null, message: sales[0] });
+
+    await saleController.getByID(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(sales[0]);
   });
 });
